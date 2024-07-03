98507067e90051f37ff4cabacf0ff2f97a18997a
Jacek programmer <jacek301@gmail.com>
2024-07-03 18:29:10 +0200

    Cat and Mouse - Easy Version kata solution

--- src/main/kotlin/ParseInCharProblem.kt
+++ src/main/kotlin/ParseInCharProblem.kt
@@ -1,5 +1,8 @@
 package catmouse
 
 fun catMouse(s: String): String {
-    return "";
+
+    val dotCount = s.count { c: Char -> c == '.' }
+
+    return if (dotCount <= 3) "Caught!" else "Escaped!"
 }
