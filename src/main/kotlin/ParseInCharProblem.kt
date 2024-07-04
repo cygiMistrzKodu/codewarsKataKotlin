961ef0b6a784a19a628557ebed503af48fb1bd50
Jacek programmer <jacek301@gmail.com>
2024-07-04 20:40:32 +0200

    Going to the cinema kata solution

--- src/main/kotlin/ParseInCharProblem.kt
+++ src/main/kotlin/ParseInCharProblem.kt
@@ -1,5 +1,25 @@
 package going
 
+import kotlin.math.ceil
+import kotlin.math.pow
+
 fun movie(card:Int, ticket:Int, perc:Double):Int {
-    return 0;
+
+    var systemA: Int
+    var systemB: Double
+    var sumOfTicketWithDiscount  = 0.0
+
+    var day = 0
+
+    do {
+
+        day++
+
+        systemA = ticket * day
+        sumOfTicketWithDiscount += ticket * perc.pow(day.toDouble())
+        systemB = card + sumOfTicketWithDiscount
+
+    } while ( ceil(systemB) >= systemA)
+
+    return day;
 }
