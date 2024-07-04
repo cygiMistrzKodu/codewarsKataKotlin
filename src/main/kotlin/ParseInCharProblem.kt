97f776512e79dd36b27828da7684795301cbc9c2
Jacek programmer <jacek301@gmail.com>
2024-07-05 01:46:51 +0200

    Codewars Leaderboard Climber kata solution. In kata is issue with description. Should be plus one I think.

--- src/main/kotlin/ParseInCharProblem.kt
+++ src/main/kotlin/ParseInCharProblem.kt
@@ -1,7 +1,29 @@
 package com.codewars.apartridge
 
+import kotlin.math.floor
+
 object CodewarsLeaderboardClimber {
-    fun leaderBoard(user:String, userScore:Int, yourScore:Int):String {
-        return "show me the code!"
+    fun leaderBoard(user: String, userScore: Int, yourScore: Int): String {
+
+        if (yourScore > userScore) {
+            return "Winning!"
+        }
+
+        if (yourScore == userScore) {
+            return "Only need one!"
+
+        }
+
+        val pointsToEven = userScore - yourScore
+        val betaKata = (floor(((pointsToEven) / 3).toDouble())).toInt()
+        val eightKyuKata = (pointsToEven - betaKata * 3)
+
+        var dammit = ""
+
+        if (betaKata + eightKyuKata > 1000) {
+            dammit = " Dammit!"
+        }
+
+        return "To beat $user's score, I must complete $betaKata Beta kata and $eightKyuKata 8kyu kata.$dammit"
     }
 }
