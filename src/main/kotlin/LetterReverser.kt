e7c111c4b17990d285f6dfbc62652b355a1c1374
Jacek programmer <jacek301@gmail.com>
2024-07-06 19:31:57 +0200

    Deodorant Evaporator kata solution

--- src/main/kotlin/LetterReverser.kt
+++ src/main/kotlin/LetterReverser.kt
@@ -1,3 +1,16 @@
 fun evaporator(content: Double, evap_per_day: Double, threshold: Double): Int {
-    return -1;
+
+    val contentThreshold = (threshold / 100) * content
+    var dayEvaporatorUseless = 0;
+    var contentLostEachDay = content;
+    val percentLost = evap_per_day / 100
+
+    do {
+
+        contentLostEachDay -= contentLostEachDay * percentLost
+        dayEvaporatorUseless++
+
+    } while (contentThreshold < contentLostEachDay)
+
+    return dayEvaporatorUseless
 }
