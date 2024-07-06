17f27f3a42607df69547bfaed2f848b0ef99956f
Jacek programmer <jacek301@gmail.com>
2024-07-06 02:11:22 +0200

    Simple Fun #384: Is Turing's Equation? kata solution

--- src/main/kotlin/LetterReverser.kt
+++ src/main/kotlin/LetterReverser.kt
@@ -1,4 +1,11 @@
 fun isTuringEquation(s: String): Boolean {
 
-    return false;
+    val numbersList =
+        "(\\d+)\\+(\\d+)=(\\d+)".toRegex().find(s)?.groupValues?.subList(1, 4)?.map { it.reversed().toDouble() }
+
+    if (numbersList != null) {
+        return numbersList[0] + numbersList[1] == numbersList[2]
+    }
+
+    return false
 }
