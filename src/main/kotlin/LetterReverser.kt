fb606c4f8f3384b28444b5594920eab1cca8d9ea
Jacek programmer <jacek301@gmail.com>
2024-07-08 21:09:57 +0200

    Count the Digit kata solution

--- src/main/kotlin/LetterReverser.kt
+++ src/main/kotlin/LetterReverser.kt
@@ -1,5 +1,26 @@
 package countdig
 
-fun nbDig(n:Int, d:Int):Int {
-    return 0
+fun countDigitInNumber(number: Int, digit: Int): Int {
+    var temp = number
+    var digitCount = 0
+    while (temp != 0) {
+        val nextDigit = temp % 10
+        if (nextDigit == digit) {
+            digitCount++
+        }
+        temp /= 10
+    }
+    return digitCount
 }
+
+fun nbDig(n: Int, d: Int): Int {
+
+    var digitCount  = if (d == 0) 1 else 0
+    for (number in 0..n) {
+        digitCount += countDigitInNumber(number * number, d)
+    }
+    return digitCount
+
+
+}
+
