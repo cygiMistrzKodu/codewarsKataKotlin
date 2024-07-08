4c15a2db725eb29a1f5609367c5de85021ae1ad4
Jacek programmer <jacek301@gmail.com>
2024-07-08 16:19:09 +0200

    Hero's root kata solution

--- src/main/kotlin/LetterReverser.kt
+++ src/main/kotlin/LetterReverser.kt
@@ -1,5 +1,29 @@
 package hero
 
+import kotlin.math.abs
+
 fun intRac(n: Long, guess: Long): Long {
-    return  1;
+
+    var aproximation = guess
+    val e = 1
+
+    val approximations: MutableList<Long> = mutableListOf()
+    approximations.add(guess)
+    var index = 0
+
+    do {
+
+        aproximation = (aproximation + (n / aproximation)) / 2
+
+        approximations.add(aproximation)
+
+        index++
+
+    } while (abs(approximations[index - 1] - approximations[index]) > e)
+
+    if (approximations[index] == approximations[index - 1]) {
+        return approximations.count().toLong() - 1
+    }
+
+    return approximations.count().toLong()
 }
--- src/test/kotlin/TestSolution.kt
+++ src/test/kotlin/TestSolution.kt
@@ -9,6 +9,8 @@ class IntSqRootTest {
         println("Fixed Tests")
         assertEquals(4, intRac(25, 1));
         assertEquals(3, intRac(125348, 300));
+        assertEquals(5, intRac(16000, 400));
+        assertEquals(16, intRac(11409767, 1));
 
     }
 
