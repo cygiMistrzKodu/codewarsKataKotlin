cd972089572890b63e63707fa5cca11cdb4c21f6
Jacek programmer <jacek301@gmail.com>
2024-07-09 17:17:14 +0200

    Find Screen Size kata start

--- src/main/kotlin/LetterReverser.kt
+++ src/main/kotlin/LetterReverser.kt
@@ -1,22 +1,3 @@
-import java.util.*
-
-object FixStringCase {
-
-    fun solve(s: String): String {
-
-        val lowerCaseLetterCount = s.count { it.isLowerCase() }
-        val upperCaseLetterCount = s.count { it.isUpperCase() }
-
-        if (lowerCaseLetterCount == upperCaseLetterCount) {
-            return s.lowercase()
-        }
-
-        if (lowerCaseLetterCount > upperCaseLetterCount) {
-            return s.lowercase()
-
-        }
-
-        return s.uppercase()
-    }
-
+fun findScreenHeight(width: Int, ratio: String): String {
+    return "Happy coding :)"
 }
