97e94a65ad32b4ee268f0037e6a9a09e55079f40
Jacek programmer <jacek301@gmail.com>
2024-07-11 20:16:26 +0200

    Geometric Progression Sequence kata solution

--- src/main/kotlin/LetterReverser.kt
+++ src/main/kotlin/LetterReverser.kt
@@ -1,1 +1,11 @@
-fun geometricSequenceElements(a: Int, r: Int, n: Int): String = TODO()
+fun geometricSequenceElements(a: Int, r: Int, n: Int): String {
+
+    var sequenceString = ""
+    var nextNumber = a
+    for (number in 0..n - 2) {
+        sequenceString += "$nextNumber, "
+        nextNumber *= r
+    }
+
+    return sequenceString + nextNumber
+}
