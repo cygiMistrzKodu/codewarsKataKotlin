eb5202c5ef6b7bd8ff181ddee65ede3a757dad8f
Jacek programmer <jacek301@gmail.com>
2024-07-12 00:27:41 +0200

    The Office I - Outed kata start and solution

--- src/main/kotlin/LetterReverser.kt
+++ src/main/kotlin/LetterReverser.kt
@@ -1,4 +1,9 @@
-fun geometricSequenceElements(a: Int, r: Int, n: Int): String =
-    generateSequence(a) { it * r }
-        .take(n)
-        .joinToString(", ")
+fun outed(meet: Map<String, Int>, boss: String): String {
+
+    val sumNoBoss = meet.values.reduce { sum, happinessRating -> sum + happinessRating }
+    val bossEntry = meet.entries.first { it.key == boss }
+    val sumWithBoss = sumNoBoss + bossEntry.value
+    val overallHappiness = sumWithBoss / meet.count()
+
+    return if (overallHappiness <= 5) "Get Out Now!" else "Nice Work Champ!"
+}
