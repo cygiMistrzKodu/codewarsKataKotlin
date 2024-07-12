8a71a5e78cde926cf7ffd0b85f54825512eaee59
Jacek programmer <jacek301@gmail.com>
2024-07-12 19:23:36 +0200

    Mutual Recursion kata solution

--- src/main/kotlin/LetterReverser.kt
+++ src/main/kotlin/LetterReverser.kt
@@ -1,7 +1,15 @@
-fun f(n : Int) : Int {
-    TODO()
+fun f(n: Int): Int {
+
+    if (n == 0) {
+        return 1
+    }
+    return n - m(f(n - 1))
 }
 
-fun m(n : Int) : Int {
-    TODO()
+fun m(n: Int): Int {
+
+    if (n == 0) {
+        return 0
+    }
+    return n - f(m(n - 1))
 }
