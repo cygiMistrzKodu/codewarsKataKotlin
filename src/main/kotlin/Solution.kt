a55ae50c60715899868d8b45e92d1bed4f956120
Jacek programmer <jacek301@gmail.com>
2024-07-14 01:25:16 +0200

    Best travel kata start

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,21 +1,6 @@
-fun babyCount(x: String): Int? {
+package besttravel
 
-    val babyLetters = listOf('a', 'b', 'y')
-
-    val babyLetterCountMap = x.map { it.lowercaseChar() }.filter { babyLetters.contains(it) }.groupBy { it }
-        .mapValues { (_, value) -> value.size }
-
-    var letterACount = babyLetterCountMap['a'] ?: 0
-    var letterBCount = babyLetterCountMap['b'] ?: 0
-    var letterYCount = babyLetterCountMap['y'] ?: 0
-
-    var babiesCount = 0;
-    while (letterBCount > 1 && letterACount > 0 && letterYCount > 0) {
-        letterACount--
-        letterBCount -= 2
-        letterYCount--
-        babiesCount++
-    }
-
-    return if (babiesCount == 0) null else babiesCount
+fun chooseBestSum(t:Int, k:Int, ls:List<Int>):Int {
+    // your code
+    return 0
 }
--- src/test/kotlin/TestSolution.kt
+++ src/test/kotlin/TestSolution.kt
@@ -1,18 +1,20 @@
-import kotlin.test.assertEquals
+package besttravel
+
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
-class TestExample {
+class SumOfKTest {
+    //----------------
     @Test
-    fun testFixed() {
-        assertEquals(1, babyCount("baby"))
-        assertEquals(1, babyCount("abby"))
-        assertEquals(3, babyCount("baby baby baby"))
-        assertEquals(1, babyCount("Why the hell are there so many babies?!"))
-        assertEquals(1, babyCount("Anyone remember life before babies?"))
-        assertEquals(2, babyCount("Happy babies boom ba by?"))
-        assertEquals(1, babyCount("b a b y"))
-        assertEquals(null, babyCount(""))
-        assertEquals(null, babyCount("none her"))
-        assertEquals(7, babyCount("vbawo gnbcvavG yaBb fnaa bsGdyaSB aBkG yBASby pGnytAv bsymbbyv nmvJya bmaw sSyisk AynfxBb yyAA Sari aNaa"))
+    fun BasicTests1() {
+        println("****** Basic Tests small numbers******")
+        var ts = listOf(50, 55, 56, 57, 58)
+        var n = chooseBestSum(163, 3, ts)
+        assertEquals(163, n.toLong())
+        ts = listOf(50)
+        var m = chooseBestSum(163, 3, ts)
+        assertEquals(-1, m)
+
     }
+
 }
