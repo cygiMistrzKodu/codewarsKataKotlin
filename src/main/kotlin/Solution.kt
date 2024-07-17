547ae8b07c865594c56c2852e6b1f1e0c5fb2638
Jacek programmer <jacek301@gmail.com>
2024-07-17 22:51:30 +0200

    Best travel kata solution

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,6 +1,33 @@
 package besttravel
 
-fun chooseBestSum(t:Int, k:Int, ls:List<Int>):Int {
-    // your code
-    return 0
+fun chooseBestSum(t: Int, k: Int, ls: List<Int>): Int {
+
+    val townCombinations = mutableListOf<List<Int>>()
+
+    fun generateCombinations(currentCombination: List<Int>, startIndex: Int) {
+
+        if (currentCombination.size == k) {
+            townCombinations.add(currentCombination.toList())
+            return
+        }
+
+        for (i in startIndex until ls.size) {
+            generateCombinations(currentCombination + ls[i], i + 1)
+        }
+    }
+
+    generateCombinations(emptyList(), 0)
+
+    var bestSum = -1
+
+    townCombinations.forEach {
+
+        val sumTownGroup = it.sum()
+
+        if (sumTownGroup in (bestSum + 1)..t) {
+            bestSum = sumTownGroup
+        }
+    }
+
+    return bestSum
 }
--- src/test/kotlin/TestSolution.kt
+++ src/test/kotlin/TestSolution.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test
 class SumOfKTest {
     //----------------
     @Test
-    fun BasicTests1() {
+    fun basicTests1() {
         println("****** Basic Tests small numbers******")
         var ts = listOf(50, 55, 56, 57, 58)
         var n = chooseBestSum(163, 3, ts)
@@ -15,6 +15,22 @@ class SumOfKTest {
         var m = chooseBestSum(163, 3, ts)
         assertEquals(-1, m)
 
+    }
+
+    @Test
+    fun basicTestsAdditional() {
+        println("****** Basic Tests my******")
+        var ts = listOf(91, 74, 73, 85, 73, 81, 87)
+        var n = chooseBestSum(331, 4, ts)
+        assertEquals(331, n.toLong())
+    }
+
+    @Test
+    fun basicTestsWhenShouldBeMinusOne() {
+        println("****** when should be -1 ******")
+        var ts = listOf(91, 74, 73, 85, 73, 81, 87)
+        var n = chooseBestSum(331, 5, ts)
+        assertEquals(-1, n.toLong())
     }
 
 }
