6638ae297094604dc38deee2b9fcdf959cf9ac57
Jacek programmer <jacek301@gmail.com>
2024-07-18 21:26:39 +0200

    English beggars kata solution

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,3 +1,22 @@
 fun beggars(values: List<Int>, n: Int): List<Int> {
-    return listOf(0)
+
+    val beggarsSum = MutableList(n) { 0 }
+
+    if (beggarsSum.isEmpty()) {
+        return emptyList()
+    }
+
+    var beggarIndex = 0
+    values.forEach {
+
+        if (beggarIndex >= beggarsSum.count()) {
+            beggarIndex = 0
+        }
+
+        beggarsSum[beggarIndex] += it
+        beggarIndex++
+
+    }
+
+    return beggarsSum
 }
