5421e407874935a78a7f6e0e8c54a3f646001faf
Jacek programmer <jacek301@gmail.com>
2024-09-03 00:47:24 +0200

    Speed Control kata solution

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,5 +1,14 @@
 package gps
 
-fun gps(s:Int, x:DoubleArray):Int {
-    return 0
+import kotlin.math.floor
+
+fun gps(s: Int, x: DoubleArray): Int {
+
+    if (x.size <= 1) {
+        return 0
+    }
+
+    val biggestDiff = x.asSequence().zipWithNext { a, b -> b - a }.max()
+
+    return floor(3600 * biggestDiff / s).toInt()
 }
--- src/test/kotlin/TestSolution.kt
+++ src/test/kotlin/TestSolution.kt
@@ -12,6 +12,8 @@ class GpsSpeedTest {
         testing(gps(20, x), 41)
         x = doubleArrayOf(0.0, 0.11, 0.22, 0.33, 0.44, 0.65, 1.08, 1.26, 1.68, 1.89, 2.1, 2.31, 2.52, 3.25)
         testing(gps(12, x), 219)
+        x = doubleArrayOf()
+        testing(gps(19, x), 0)
 
     }
     companion object {
