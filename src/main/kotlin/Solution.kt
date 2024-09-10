f46b07be072a137af374b41f53580ab8416eed88
Jacek programmer <jacek301@gmail.com>
2024-09-10 20:05:39 +0200

    up AND down kata solution

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,5 +1,53 @@
 package updown
 
-fun arrange(strng:String):String {
-    return ""
+fun arrange(strng: String): String {
+
+    val words = strng.split(" ").toMutableList()
+
+    for (index in words.indices) {
+
+        if (index < words.size - 1) {
+            if (isOrderEven(index) && isNextWordShorter(words, index)) {
+                moveWordOneStepLeft(words, index)
+            }
+            if (isOrderOdd(index) && isNextWordLonger(words, index)) {
+                moveWordOneStepRight(words, index)
+            }
+        }
+    }
+
+    return makeDownAndUp(words).joinToString(" ")
+}
+
+private fun makeDownAndUp(words: MutableList<String>) = words.mapIndexed { index, word ->
+
+    if (isOrderOdd(index)) {
+        word.uppercase()
+    } else {
+        word.lowercase()
+    }
+}
+
+private fun isOrderEven(index: Int) = index % 2 == 0
+
+private fun isNextWordShorter(words: MutableList<String>, index: Int) =
+    words[index].length > words[index + 1].length
+
+private fun moveWordOneStepRight(words: MutableList<String>, index: Int) {
+    swapPlaces(words, index)
+}
+
+private fun isOrderOdd(index: Int) = index % 2 == 1
+
+private fun isNextWordLonger(words: MutableList<String>, index: Int) =
+    words[index].length < words[index + 1].length
+
+private fun moveWordOneStepLeft(words: MutableList<String>, index: Int) {
+    swapPlaces(words, index)
+}
+
+private fun swapPlaces(words: MutableList<String>, index: Int) {
+    val word = words[index]
+    words[index] = words[index + 1]
+    words[index + 1] = word
 }
--- src/test/kotlin/TestSolution.kt
+++ src/test/kotlin/TestSolution.kt
@@ -4,13 +4,36 @@ package updown
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
-private fun testing(actual:String, expected:String) {
+private fun testing(actual: String, expected: String) {
     assertEquals(expected, actual)
 }
+
 class arrangestringMainTest {
     @Test
     fun test() {
         println("Fixed Tests updown")
+        testing(
+            arrange("who hit"),
+            "who HIT"
+        )
+        testing(
+            arrange("who h"),
+            "h WHO"
+        )
+        testing(
+            arrange("h who"),
+            "h WHO"
+        )
+        testing(
+            arrange("h www oo"),
+            "h WWW oo"
+        )
+        testing(
+            arrange("h oo www"),
+            "h WWW oo"
+        )
+
+
         testing(arrange("who hit retaining The That a we taken"),
             "who RETAINING hit THAT a THE we TAKEN") // 3
         testing(arrange("on I came up were so grandmothers"),
