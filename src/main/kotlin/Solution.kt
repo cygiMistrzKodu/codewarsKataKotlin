cb4f52ec168d74b61d52f2a31e82131ae66d0c8c
Jacek programmer <jacek301@gmail.com>
2024-09-15 21:59:06 +0200

    PI approximation kata start

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,18 +1,7 @@
-package bouncing
+package solution
 
-fun bouncingBall(h: Double, bounce: Double, window: Double): Int {
-
-    if (h <= 0 || window >= h || bounce <= 0 || bounce >= 1) {
-        return -1
+object PiApprox {
+    fun iterPi2String(epsilon: Double): String {
+        return ""
     }
-
-    var timesBallSeen = 0;
-    var ballHeight = h
-    while (ballHeight > window) {
-        timesBallSeen += 2
-        ballHeight *= bounce
-    }
-
-
-    return timesBallSeen - 1
 }
--- src/test/kotlin/TestSolution.kt
+++ src/test/kotlin/TestSolution.kt
@@ -1,47 +1,20 @@
-package bouncing
+package solution
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 
-class BouncingBallTest {
-    @Test
-    fun heightZeroThenMinusOne() {
-        assertEquals(-1, bouncingBall(0.0, 0.66, 1.5))
-    }
+class  PiApproxTest {
 
-    @Test
-    fun boundLessThanEqualZeroThenMinusOne() {
-        assertEquals(-1, bouncingBall(3.0, 0.0, 1.5))
+    private fun testing(epsilon: Double, expect: String) {
+        val actual: String = PiApprox.iterPi2String(epsilon)
+        assertEquals(expect, actual)
     }
+    @Test
+    fun basicTests() {
+        testing(0.1, "[10, 3.0418396189]")
+        testing(0.01, "[100, 3.1315929036]")
+        testing(0.001, "[1000, 3.1405926538]")
 
-    @Test
-    fun boundGraterThanOneThenMinusOne() {
-        assertEquals(-1, bouncingBall(3.0, 1.2, 1.5))
     }
-
-    @Test
-    fun windowsParameterEqualHeightThenMinusOne() {
-        assertEquals(-1, bouncingBall(3.0, 1.2, 3.0))
-    }
-
-    @Test
-    fun windowsParameterGreaterThanHeightTHenMinusOne() {
-        assertEquals(-1, bouncingBall(3.0, 0.8, 3.5))
-    }
-
-    @Test
-    fun test1() {
-        assertEquals(3, bouncingBall(3.0, 0.66, 1.5))
-    }
-    @Test
-    fun test2() {
-        assertEquals(15, bouncingBall(30.0, 0.66, 1.5))
-    }
-
-    @Test
-    fun test3() {
-        assertEquals(1, bouncingBall(2.0, 0.5, 1.0))
-    }
-
 }
