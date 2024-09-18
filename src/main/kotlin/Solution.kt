1e33704312d254a256052ebc1f560050b6beeb02
Jacek programmer <jacek301@gmail.com>
2024-09-18 17:37:42 +0200

    PI approximation kata solution

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,7 +1,21 @@
 package solution
 
+import java.util.Locale
+import kotlin.math.abs
+import kotlin.math.round
+
 object PiApprox {
     fun iterPi2String(epsilon: Double): String {
-        return ""
+
+        var iteration = 0.0
+        var piApproximation = 0.0
+
+        while (abs(piApproximation - Math.PI) >= epsilon) {
+            piApproximation += 4 * Math.pow(-1.0, iteration) / (2 * iteration + 1)
+            iteration++
+        }
+
+        return "[${iteration.toInt()}, ${String.format(Locale.US, "%.10f", piApproximation)}]"
     }
+
 }
