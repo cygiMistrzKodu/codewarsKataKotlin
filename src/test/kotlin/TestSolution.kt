37982126e4c60271975f5c0029f67988528959c3
Jacek programmer <jacek301@gmail.com>
2024-09-24 17:14:12 +0200

    Easy Diagonal kata solution not optimal solution. Add few test to check.

--- src/test/kotlin/TestSolution.kt
+++ src/test/kotlin/TestSolution.kt
@@ -114,4 +114,24 @@ class DiagonalTest {
         assertEquals(BigInteger.valueOf(54264), Diagonal.diagonal(20, 5))
 
     }
+
+    @Test
+    fun longNumbersTests() {
+        assertEquals(BigInteger.valueOf(6385476296235036), Diagonal.diagonal(1291, 5))
+    }
+
+    @Test
+    fun veryLongNumberTest() {
+
+        assertEquals(
+            BigInteger(
+                "15520018282794855145521771584008670616528023199516300148225151327498554518" +
+                        "946739091210873083654062335657029403538513437778616308444771447785680961989809223546226398042673172" +
+                        "9793794728652023023749985526341895119496429851855415" +
+                        "24070069692166648244838032400"
+            ), Diagonal.diagonal(5720, 120)
+        )
+
+
+    }
 }
