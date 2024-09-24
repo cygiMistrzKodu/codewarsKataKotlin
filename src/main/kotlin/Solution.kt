d9a6ff944a20f4b830dcb1b0d49ef8caa5ab527f
Jacek programmer <jacek301@gmail.com>
2024-09-24 16:42:01 +0200

    Easy Diagonal kata solution not optimal solution. on code wars.

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -6,7 +6,40 @@ object Diagonal {
 
     fun diagonal(n: Int, p: Int): BigInteger {
 
-        val bigInteger = BigInteger.valueOf(3)
-        return bigInteger
+        val pascalTriangle = createPascalTriangle(n + 1)
+
+        return  (p..n).map { index ->
+            pascalTriangle[index][p]
+
+        }.reduce { sum, number -> sum + number }
+
+
     }
+
+    fun createPascalTriangle(levels: Int): List<List<BigInteger>> {
+
+        val pascalTriangle: MutableList<MutableList<BigInteger>> = mutableListOf()
+
+        if (levels >= 1) {
+            pascalTriangle.add(mutableListOf(BigInteger.ONE))
+        }
+
+        if (levels >= 2) {
+            pascalTriangle.add(mutableListOf(BigInteger.ONE, BigInteger.ONE))
+        }
+
+        if (levels >= 3) {
+
+            (3..levels).map {
+                val nextLevel =
+                    listOf(BigInteger.ONE) + pascalTriangle.last()
+                        .zipWithNext() { a, b -> a + b } + listOf(BigInteger.ONE)
+                pascalTriangle.add(nextLevel.toMutableList())
+
+            }
+        }
+        return pascalTriangle
+
+    }
+
 }
--- src/test/kotlin/TestSolution.kt
+++ src/test/kotlin/TestSolution.kt
@@ -6,6 +6,107 @@ import kotlin.test.assertEquals
 
 class DiagonalTest {
 
+
+    @Test
+    fun whenLineOneThenPascalTriangleHaveOne() {
+        assertEquals(
+            mutableListOf(
+                mutableListOf(BigInteger.ONE)
+            ), Diagonal.createPascalTriangle(1)
+        )
+    }
+
+    @Test
+    fun whenLineOneThenPascalTriangleHaveTwoLevels() {
+        assertEquals(
+            mutableListOf(
+                mutableListOf(BigInteger.ONE),
+                mutableListOf(BigInteger.ONE, BigInteger.ONE)
+            ), Diagonal.createPascalTriangle(2)
+        )
+    }
+
+    @Test
+    fun pascalTriangleLevelsThree() {
+        assertEquals(
+            mutableListOf(
+                mutableListOf(BigInteger.ONE),
+                mutableListOf(BigInteger.ONE, BigInteger.ONE),
+                mutableListOf(BigInteger.ONE, BigInteger.TWO, BigInteger.ONE)
+            ), Diagonal.createPascalTriangle(3)
+        )
+    }
+
+    @Test
+    fun pascalTriangleLevelsFour() {
+        assertEquals(
+            mutableListOf(
+                mutableListOf(BigInteger.ONE),
+                mutableListOf(BigInteger.ONE, BigInteger.ONE),
+                mutableListOf(BigInteger.ONE, BigInteger.TWO, BigInteger.ONE),
+                mutableListOf(
+                    BigInteger.ONE, BigInteger.valueOf(3),
+                    BigInteger.valueOf(3), BigInteger.ONE
+                )
+            ), Diagonal.createPascalTriangle(4)
+        )
+    }
+
+    @Test
+    fun pascalTriangleLevelsFive() {
+        assertEquals(
+            mutableListOf(
+                mutableListOf(BigInteger.ONE),
+                mutableListOf(BigInteger.ONE, BigInteger.ONE),
+                mutableListOf(BigInteger.ONE, BigInteger.TWO, BigInteger.ONE),
+                mutableListOf(
+                    BigInteger.ONE, BigInteger.valueOf(3),
+                    BigInteger.valueOf(3), BigInteger.ONE
+                ),
+                mutableListOf(
+                    BigInteger.ONE, BigInteger.valueOf(4), BigInteger.valueOf(6), BigInteger.valueOf(4),
+                    BigInteger.ONE
+                )
+            ), Diagonal.createPascalTriangle(5)
+        )
+    }
+
+
+    @Test
+    fun when1And0ThenDiagonalSum2() {
+        assertEquals(BigInteger.valueOf(2), Diagonal.diagonal(1, 0))
+    }
+
+    @Test
+    fun when2And0ThenDiagonalSum3() {
+        assertEquals(BigInteger.valueOf(3), Diagonal.diagonal(2, 0))
+    }
+
+    @Test
+    fun when3And0ThenDiagonalSum4() {
+        assertEquals(BigInteger.valueOf(4), Diagonal.diagonal(3, 0))
+    }
+
+    @Test
+    fun when2And1ThenDiagonalSum3() {
+        assertEquals(BigInteger.valueOf(3), Diagonal.diagonal(2, 1))
+    }
+
+    @Test
+    fun when3And1ThenDiagonalSum6() {
+        assertEquals(BigInteger.valueOf(6), Diagonal.diagonal(3, 1))
+    }
+
+    @Test
+    fun when3And2ThenDiagonalSum4() {
+        assertEquals(BigInteger.valueOf(4), Diagonal.diagonal(3, 2))
+    }
+
+    @Test
+    fun when4And0ThenDiagonalSum4() {
+        assertEquals(BigInteger.valueOf(5), Diagonal.diagonal(4, 0))
+    }
+
     @Test
     fun basicTests() {
         assertEquals(BigInteger.valueOf(5985), Diagonal.diagonal(20, 3))
