fe2b1b03a0628626091a210216d038023469a35b
Jacek programmer <jacek301@gmail.com>
2024-09-26 22:55:40 +0200

    Easy Diagonal kata solution From CodeWars 3

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -6,13 +6,12 @@ object Diagonal {
 
     fun diagonal(n: Int, p: Int): BigInteger {
 
-        var r = BigInteger.valueOf(1L)
-        var i = BigInteger.valueOf(1L)
-        while (i <= BigInteger.valueOf((p + 1).toLong())) {
-            r = r * (BigInteger.valueOf(n.toLong()) - i + BigInteger.valueOf(2L)) / i
-            i = i + BigInteger.valueOf(1L)
+        var result = BigInteger.ONE
+        for (i in 0L..p.toLong()) {
+            result *= BigInteger.valueOf(n - i + 1)
+            result /= BigInteger.valueOf(i + 1)
         }
-        return r
+        return result
     }
 
 }
