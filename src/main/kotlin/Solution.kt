ea46e6a61c11f9773e53b98df169b3ea69447039
Jacek programmer <jacek301@gmail.com>
2024-09-28 00:38:29 +0200

    Alphabet war kata solution

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,3 +1,35 @@
 fun alphabetWar(fight: String): String {
-   return ""
+
+    val leftPower = mapOf(
+        "w" to 4,
+        "p" to 3,
+        "b" to 2,
+        "s" to 1
+    )
+
+    val rightPower = mapOf(
+        "m" to 4,
+        "q" to 3,
+        "d" to 2,
+        "z" to 1
+
+    )
+
+    val leftScore = calculateScore(fight, leftPower)
+    val rightScore = calculateScore(fight, rightPower)
+
+    if (leftScore > rightScore) {
+        return "Left side wins!"
+    }
+
+    if (leftScore < rightScore) {
+        return "Right side wins!"
+    }
+
+    return "Let's fight again!"
+
 }
+
+private fun calculateScore(fight: String, powers: Map<String, Int>) =
+    fight.split("").filter { powers.containsKey(it) }.mapNotNull { powers[it] }
+        .fold(0) { sum, power -> sum + power }
--- src/test/kotlin/TestSolution.kt
+++ src/test/kotlin/TestSolution.kt
@@ -6,8 +6,8 @@ class TestExample {
     @Test
     fun testFixed() {
         assertEquals("Right side wins!", alphabetWar("z"))
-        assertEquals("Let's fight again!", alphabetWar("zdqmwpbs"))
         assertEquals("Right side wins!", alphabetWar("zzzzs"))
         assertEquals("Left side wins!", alphabetWar("wwwwww"))
+        assertEquals("Let's fight again!", alphabetWar("zdqmwpbs"))
     }
 }
