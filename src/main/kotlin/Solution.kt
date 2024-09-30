8261796061a14a34e636e5e95345cb77f7ddee3b
Jacek programmer <jacek301@gmail.com>
2024-09-30 03:04:09 +0200

    Looking for a benefactor kata solution from codeWars

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,23 +1,11 @@
 package newavg
 
+import kotlin.math.ceil
+import kotlin.math.roundToLong
+
 fun newAvg(a: DoubleArray, navg: Double): Long {
 
-    var average = if (a.isNotEmpty()) a.sum() / a.count() else 0.0
-
-    if (average >= navg) {
-        throw IllegalArgumentException()
-    }
-
-    var nextDonation = 0L
-    val beforeDonationSum = a.sum()
-    val nextDonationCount = a.count() + 1
-
-    while (average < navg) {
-
-        nextDonation++
-        average = (beforeDonationSum + nextDonation) / nextDonationCount
-
-    }
-
-    return nextDonation
+    val x = navg*(a.size + 1) - a.sum()
+    require(x >= 0)
+    return ceil(x).roundToLong()
 }
