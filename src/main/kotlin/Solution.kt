76067995db429910126884f97fba8683b56bd25a
Jacek programmer <jacek301@gmail.com>
2024-09-30 16:49:10 +0200

    London CityHacker kata solution

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,6 +1,29 @@
 package codewars.cityhacker
 
+import java.util.Locale
+
 fun londonCityHacker(journey: Array<Any>): String {
 
-    return ""
+    var cost = 0.0
+    var bussAdjacent = 0
+    for (connection in journey) {
+        when (connection) {
+            is String -> {
+                cost += 2.4
+                bussAdjacent = 0
+            }
+
+            is Number -> {
+                if (bussAdjacent == 0) {
+                    cost += 1.5
+                }
+                bussAdjacent++
+                if (bussAdjacent >= 2) {
+                    bussAdjacent = 0
+                }
+            }
+        }
+    }
+
+    return "£${String.format(Locale.US, "%.2f", cost)}"
 }
