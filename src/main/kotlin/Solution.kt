5863f76f80c8a1e6610c768a11523d47ab060af6
Jacek programmer <jacek301@gmail.com>
2024-10-03 02:21:43 +0200

    Floating-point Approximation (II) kata solution

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,7 +1,15 @@
 package approxfloat
 
-//f: (Double) -> Double   ( moge tu funkcję wstawić
-fun interp(..., l:Double, u:Double, n:Int):List<Double> {
-    // replace ... by a function f: Double -> Double
-    // Your code
+import kotlin.math.floor
+
+fun interp(f: (Double) -> Double, l: Double, u: Double, n: Int): List<Double> {
+
+    return generateSequence(0) { it + 1 }
+        .takeWhile { it < n }
+        .map {
+            (l + it) * (u - l) / n
+        }
+        .map { f.invoke(it) }
+        .map { floor(it * 100.0) / 100.0 }
+        .toList()
 }
