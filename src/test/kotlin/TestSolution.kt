d56bc3806946b1353cc193f17dbe6a4e08c27bd4
Jacek programmer <jacek301@gmail.com>
2024-10-05 23:46:18 +0200

    Alphabet war - airstrike - letters massacre kata solution

--- src/test/kotlin/TestSolution.kt
+++ src/test/kotlin/TestSolution.kt
@@ -8,8 +8,8 @@ class TestExample {
         assertEquals("Let's fight again!", alphabetWar("****"))
         assertEquals("Let's fight again!", alphabetWar("z*dq*mw*pb*s"))
         assertEquals("Let's fight again!", alphabetWar("zdqmwpbs"))
-        assertEquals("Right side wins!", alphabetWar("zz*zzs"))
-        assertEquals("Left side wins!", alphabetWar("sz**z**zs"))
+        assertEquals("Right side wins!", alphabetWar("zzs"))
+        assertEquals("Left side wins!", alphabetWar("sz**zz*z**zs"))
         assertEquals("Left side wins!", alphabetWar("z*z*z*zs"))
         assertEquals("Left side wins!", alphabetWar("*wwwwww*z*"))
     }
