f40b956e09f2f08d9ba4e3ce1ad88461fb166dfd
Jacek programmer <jacek301@gmail.com>
2024-10-10 18:53:29 +0200

    Financing a purchase kata solution

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,9 +1,51 @@
 package solution
 
+import kotlin.math.pow
+import kotlin.math.round
+
 object Finance {
 
     fun amort(rate: Double, bal: Int, term: Int, num_payments: Int): String {
-        // your code
-        return  ""
+
+        val c = calculateMonthlyPayment(rate, bal, term)
+        val (princ, int, newBalance) = interestPrincNewBalance(bal, rate, num_payments, c)
+
+        return "num_payment $num_payments c ${round(c).toInt()} princ " +
+                "${round(princ).toInt()} int ${round(int).toInt()} balance ${
+            round(
+                newBalance
+            ).toInt()
+        }"
+    }
+
+    fun calculateMonthlyPayment(rate: Double, bal: Int, term: Int): Double {
+
+        val r = (rate / 100) / 12
+        val n = r * bal
+        val d = 1 - ((1 + r)).pow(-term)
+
+        return (n / d)
+    }
+
+    fun interestPrincNewBalance(
+        balance: Int,
+        rate: Double,
+        numPayment: Int,
+        monthlyPayment: Double
+    ): DoubleArray {
+
+
+        val r = (rate / 100) / 12
+        var newBalance = balance.toDouble()
+
+        var monthInterest = 0.0
+        var princ = 0.0
+        for (i in 1..numPayment) {
+            monthInterest = r * newBalance
+            princ = monthlyPayment - monthInterest
+            newBalance -= princ
+
+        }
+        return doubleArrayOf(princ, monthInterest, newBalance)
     }
 }
