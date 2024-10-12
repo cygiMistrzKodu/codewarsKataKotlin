446be710574edd556f73ed59bdb6be980147a3ce
Jacek programmer <jacek301@gmail.com>
2024-10-12 22:34:51 +0200

    Playing with digits kata solution

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,4 +1,53 @@
+import kotlin.math.pow
+
 fun digPow(n: Int, p: Int): Int {
-    // ...
-    return 0
+
+    val digits = splitOnDigits(n)
+
+    val sumRiseToToConsecutivePowers = riseToConsecutivePowers(digits, p).sum()
+
+    println(sumRiseToToConsecutivePowers)
+
+    var kCandidate = 0
+    var kMultiplyN = 0
+    var k = -1
+    while (sumRiseToToConsecutivePowers >= kMultiplyN) {
+        kMultiplyN = n * kCandidate
+
+        if (kMultiplyN == sumRiseToToConsecutivePowers) {
+            k = kCandidate
+        }
+
+        kCandidate++
+    }
+    return k
+}
+
+fun splitOnDigits(n: Int): List<Int> {
+
+    val digits = mutableListOf<Int>()
+
+    var number = n
+    while (number > 0) {
+        digits.add(number % 10)
+        number /= 10
+    }
+    digits.reverse()
+
+    return digits
+}
+
+fun riseToConsecutivePowers(digits: List<Int>, startPower: Int): List<Int> {
+
+    val consecutivePowers = makePowersAsManyAsDigits(startPower, digits.count())
+
+    return digits.asSequence().zip(consecutivePowers.asSequence()) { digit, power ->
+        digit.toDouble().pow(power.toDouble())
+    }.map { it.toInt() }.toList()
+
+
+}
+
+fun makePowersAsManyAsDigits(startPower: Int, count: Int): List<Int> {
+    return List(count) { it + startPower }
 }
--- src/test/kotlin/TestSolution.kt
+++ src/test/kotlin/TestSolution.kt
@@ -9,4 +9,27 @@ class TestExample {
         assertEquals(-1, digPow(92, 1))
         assertEquals(51, digPow(46288, 3))
     }
+
+    @Test
+    fun splitOnDigitsTest() {
+        assertEquals(listOf(1, 2, 3), splitOnDigits(123))
+        assertEquals(listOf(4, 6, 2, 8, 8), splitOnDigits(46288))
+    }
+
+    @Test
+    fun riseToConsecutivePowerTest() {
+        assertEquals(listOf(1, 4, 27), riseToConsecutivePowers(listOf(1, 2, 3), 1))
+        assertEquals(listOf(4, 125, 4096), riseToConsecutivePowers(listOf(2, 5, 8), 2))
+    }
+
+    @Test
+    fun makeConsecutivePowersFromStartToEndTest() {
+        assertEquals(listOf(2, 3, 4, 5, 6), makePowersAsManyAsDigits(2, 5))
+        assertEquals(listOf(1), makePowersAsManyAsDigits(1, 1))
+        assertEquals(listOf(0), makePowersAsManyAsDigits(0, 1))
+        assertEquals(listOf(-2, -1), makePowersAsManyAsDigits(-2, 2))
+        assertEquals(listOf(2,3,4), makePowersAsManyAsDigits(2, 3))
+    }
+
+
 }
