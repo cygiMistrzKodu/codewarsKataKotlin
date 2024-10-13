00efd8a1695f6e088fb57fdc4e21abd6975a3cac
Jacek programmer <jacek301@gmail.com>
2024-10-14 00:45:38 +0200

    Decompose double strand DNA into 6 reading frames kata start

--- src/test/kotlin/TestSolution.kt
+++ src/test/kotlin/TestSolution.kt
@@ -1,35 +1,12 @@
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
-
 class TestExample {
     @Test
-    fun testFixed() {
-        assertEquals(1, digPow(89, 1))
-        assertEquals(-1, digPow(92, 1))
-        assertEquals(51, digPow(46288, 3))
+    fun basic() {
+        assertEquals(
+            "Frame 1: AGG TGA CAC CGC AAG CCT TAT ATT AGC\nFrame 2: A GGT GAC ACC GCA AGC CTT ATA TTA GC\nFrame 3: AG GTG ACA CCG CAA GCC TTA TAT TAG C\n\nReverse Frame 1: GCT AAT ATA AGG CTT GCG GTG TCA CCT\nReverse Frame 2: G CTA ATA TAA GGC TTG CGG TGT CAC CT\nReverse Frame 3: GC TAA TAT AAG GCT TGC GGT GTC ACC T",
+            DNADecomposer.decomposeDoubleStrand("AGGTGACACCGCAAGCCTTATATTAGC")
+        )
     }
-
-    @Test
-    fun splitOnDigitsTest() {
-        assertEquals(listOf(1, 2, 3), splitOnDigits(123))
-        assertEquals(listOf(4, 6, 2, 8, 8), splitOnDigits(46288))
-    }
-
-    @Test
-    fun riseToConsecutivePowerTest() {
-        assertEquals(listOf(1, 4, 27), riseToConsecutivePowers(listOf(1, 2, 3), 1))
-        assertEquals(listOf(4, 125, 4096), riseToConsecutivePowers(listOf(2, 5, 8), 2))
-    }
-
-    @Test
-    fun makeConsecutivePowersFromStartToEndTest() {
-        assertEquals(listOf(2, 3, 4, 5, 6), makePowersAsManyAsDigits(2, 5))
-        assertEquals(listOf(1), makePowersAsManyAsDigits(1, 1))
-        assertEquals(listOf(0), makePowersAsManyAsDigits(0, 1))
-        assertEquals(listOf(-2, -1), makePowersAsManyAsDigits(-2, 2))
-        assertEquals(listOf(2,3,4), makePowersAsManyAsDigits(2, 3))
-    }
-
-
 }
