8f2004771e47fb68ae388af40b69dc5f23d3438b
Jacek programmer <jacek301@gmail.com>
2024-10-18 01:17:48 +0200

    Framed Reflection kata solution

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,4 +1,32 @@
 fun mirror(text: String): String {
-    // code here
-    return ""
+
+    val wordsReversed = text.split(" ").map { it.reversed() }.toList()
+    val longestWordCount = wordsReversed.map { it.count() }.max()
+
+    val verticalEdgesInARowCount = 4
+    val horizontalEdgeStarCount = longestWordCount + verticalEdgesInARowCount
+
+    val star = "*"
+    val horizontalEdge = star.repeat(horizontalEdgeStarCount)
+
+    val mirrorFrame = StringBuilder()
+    mirrorFrame.append(horizontalEdge)
+
+    val emptyRow = "\n"
+    mirrorFrame.append(emptyRow)
+
+    val leftEdge = "$star "
+    val rightEdge = " $star"
+
+    wordsReversed.forEach { word ->
+        mirrorFrame.append(leftEdge)
+        mirrorFrame.append(word)
+        mirrorFrame.append(" ".repeat(longestWordCount - word.count()))
+        mirrorFrame.append(rightEdge)
+        mirrorFrame.append(emptyRow)
+
+    }
+    mirrorFrame.append(horizontalEdge)
+
+    return mirrorFrame.toString()
 }
--- src/test/kotlin/TestSolution.kt
+++ src/test/kotlin/TestSolution.kt
@@ -8,4 +8,9 @@ class TestExample {
         assertEquals("*********\n* olleH *\n* dlroW *\n*********", mirror("Hello World"))
         assertEquals("************\n* srawedoC *\n************", mirror("Codewars"))
     }
+
+    @Test
+    fun `More advanced test`() {
+        assertEquals("***********\n* yiawyer *\n* oywyim  *\n* atwdvpz *\n* apzqgh  *\n***********", mirror("reywaiy miywyo zpvdwta hgqzpa"))
+    }
 }
