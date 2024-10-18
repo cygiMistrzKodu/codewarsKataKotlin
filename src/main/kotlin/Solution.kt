83f90fba5a35b21f305c0e1dc1fd39226299d5d4
Jacek programmer <jacek301@gmail.com>
2024-10-19 00:35:01 +0200

    Tricky Kotlin #0: extension constructor kata Solution z codeWars

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,5 +1,9 @@
-fun Int(s: String) = s.toInt()
-fun Long(s: String) = s.toLong()
-fun Double(s: String) = s.toDouble()
 
-// the code above cannot pass the tests. Please make them pass.
+object KotlinExtensions {
+    fun Int(s: String) = s.toInt()
+    fun Long(s: String) = s.toLong()
+    fun Double(s: String) = s.toDouble()
+}
+
+val kotlin = KotlinExtensions
+
