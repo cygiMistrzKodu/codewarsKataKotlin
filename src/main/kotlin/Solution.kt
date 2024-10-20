e0282b8f3cb4e621555938f7200ae7bfd338cace
Jacek programmer <jacek301@gmail.com>
2024-10-20 22:01:41 +0200

    A Rule of Divisibility by 13 kata my solution

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,6 +1,35 @@
 package thirteen
 
-fun thirt(n:Long):Long {
-    // your code
-    return 1L
+fun thirt(n: Long): Long {
+
+    var previousDigitCycleSum = -20L
+    var currentCycleDigitSum = sumProductsBy13DivisibilityCycle(n)
+
+    while (previousDigitCycleSum != currentCycleDigitSum) {
+        previousDigitCycleSum = currentCycleDigitSum
+        currentCycleDigitSum = sumProductsBy13DivisibilityCycle(currentCycleDigitSum)
+    }
+
+    return currentCycleDigitSum
+}
+
+
+fun sumProductsBy13DivisibilityCycle(number: Long): Long {
+
+    var sumOfCycle = 0L;
+    number.toString().reversed().toList().map { it.toString().toInt() }.forEachIndexed { index, digit ->
+        sumOfCycle += getRemindersOf13ByDigitOrder(index) * digit
+    }
+
+    return sumOfCycle
+}
+
+fun getRemindersOf13ByDigitOrder(digitOrder: Int): Int {
+
+    val remindersOfModulo13 = mapOf(
+        0 to 1, 1 to 10, 2 to 9, 3 to 12, 4 to 3, 5 to 4
+    )
+    val chooseReminder = digitOrder % 6
+
+    return remindersOfModulo13.getOrDefault(chooseReminder, 0)
 }
--- src/test/kotlin/TestSolution.kt
+++ src/test/kotlin/TestSolution.kt
@@ -16,4 +16,22 @@ class ThirteenTest {
             assertEquals(expected, actual)
         }
     }
+
+    @Test
+    fun getRemindersOf13ByDigitOrderTest() {
+        assertEquals(1, getRemindersOf13ByDigitOrder(0))
+        assertEquals(10, getRemindersOf13ByDigitOrder(1))
+        assertEquals(3, getRemindersOf13ByDigitOrder(4))
+        assertEquals(1, getRemindersOf13ByDigitOrder(6))
+        assertEquals(10, getRemindersOf13ByDigitOrder(7))
+        assertEquals(12, getRemindersOf13ByDigitOrder(15))
+        assertEquals(10, getRemindersOf13ByDigitOrder(25))
+    }
+
+    @Test
+    fun sumProductsBy13DivisibilityCycleTest() {
+        assertEquals(178, sumProductsBy13DivisibilityCycle(1234567))
+        assertEquals(87, sumProductsBy13DivisibilityCycle(178))
+        assertEquals(87, sumProductsBy13DivisibilityCycle(87))
+    }
 }
