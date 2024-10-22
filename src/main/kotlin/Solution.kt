58bfe906df02586fb24ce45a84efa9ed95d5c7bd
Jacek programmer <jacek301@gmail.com>
2024-10-22 22:01:42 +0200

    Ease the StockBroker kata start

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,22 +1,8 @@
-package simpson
+package solution
 
+object OrdersSummary {
+    fun balanceStatements(lst: String): String {
 
-private fun f(x:Double):Double {
-    return 1.5 * Math.pow(Math.sin(x), 3.0)
+        return ""
     }
-
-fun simpson(n:Int):Double {
-
-    val h = Math.PI / n
-    var s1 = 0.0
-    for (i in 1..(n / 2).toInt())
-    {
-        s1 += f((2 * i - 1) * h)
-    }
-    var s2 = 0.0
-    for (i in 1..(n / 2).toInt() - 1)
-    {
-        s2 += f(2.0 * i.toDouble() * h)
-    }
-    return Math.PI / (3 * n) * (f(0.0) + f(Math.PI) + 4 * s1 + 2 * s2)
 }
