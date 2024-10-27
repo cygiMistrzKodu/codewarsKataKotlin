fbcf6d74809edf606b649a5a01ff34b1014db655
Jacek programmer <jacek301@gmail.com>
2024-10-27 22:13:16 +0100

    Ease the StockBroker kata my solution

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,8 +1,85 @@
 package solution
 
+import kotlin.math.roundToInt
+
 object OrdersSummary {
+
+    enum class OrderType(val transaction: Regex) {
+        BUY("B\$".toRegex()), SELL("S\$".toRegex())
+    }
+
     fun balanceStatements(lst: String): String {
 
-        return ""
+        println(lst)
+
+        val ordersWrongFormatted = findWrongFormattedOrders(lst)
+
+        val buyAndSell = buyAndSellSummary(lst)
+
+        return if (ordersWrongFormatted.isEmpty() || lst.isEmpty()) {
+            buyAndSell
+        } else {
+            "${buyAndSell}; Badly formed ${ordersWrongFormatted.count()}: " +
+                    "${ordersWrongFormatted.joinToString(" ;")} ;"
+        }
+
+    }
+
+    fun findWrongFormattedOrders(clientOrders: String): List<String> {
+
+        val badlyFormattedOrders = mutableListOf<String>()
+
+        clientOrders.split(",").forEach { order ->
+
+            if (!clientOrderPattern().containsMatchIn(order)) {
+                badlyFormattedOrders.add(order.trim())
+            }
+
+        }
+
+        return badlyFormattedOrders
+    }
+
+    fun chooseRightOrders(clientOrders: String): List<String> {
+
+        val rightOrders = mutableListOf<String>()
+
+        clientOrders.split(",").forEach { order ->
+
+            if (clientOrderPattern().containsMatchIn(order)) {
+                rightOrders.add(order.trim())
+            }
+
+        }
+        return rightOrders
+    }
+
+    private fun clientOrderPattern(): Regex {
+        return "[\\w.]* \\d+ \\d+\\.\\d+ B|[\\w.]* \\d+ \\d+\\.\\d+ S".toRegex()
+    }
+
+    fun buyAndSellSummary(clientOrder: String): String {
+
+        val orders = chooseRightOrders(clientOrder)
+
+        val buyOrdersSum = sumOrders(orders, OrderType.BUY)
+
+        val sellOrdersSum = sumOrders(orders, OrderType.SELL)
+
+        return "Buy: $buyOrdersSum Sell: $sellOrdersSum"
+    }
+
+    private fun sumOrders(orders: List<String>, orderType: OrderType): Int {
+        val buyOrdersSum = orders.filter { it.contains(orderType.transaction) }.sumOf {
+            quantityAndPriceRegex().findAll(it).sumOf { matchResult ->
+                val (quantity, price) = matchResult.destructured
+                quantity.toInt() * price.toDouble()
+            }
+        }.roundToInt()
+        return buyOrdersSum
+    }
+
+    private fun quantityAndPriceRegex(): Regex {
+        return """(\d+)\s*(\d+\.\d+)""".toRegex()
     }
 }
--- src/test/kotlin/TestSolution.kt
+++ src/test/kotlin/TestSolution.kt
@@ -4,12 +4,13 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 
-class  OrdersSummaryTest {
+class OrdersSummaryTest {
 
     private fun testing(lst: String, expect: String) {
         val actual: String = OrdersSummary.balanceStatements(lst)
         assertEquals(expect, actual)
     }
+
     @Test
     fun basicTests() {
         var l = "GOOG 300 542.0 B, AAPL 50 145.0 B, CSCO 250.0 29 B, GOOG 200 580.0 S"
@@ -22,5 +23,57 @@ class  OrdersSummaryTest {
         sol = "Buy: 29499 Sell: 0"
         testing(l, sol)
 
+        l = ""
+        sol = "Buy: 0 Sell: 0"
+        testing(l, sol)
+
     }
+
+    @Test
+    fun foundBadlyFormattedOrderTest() {
+        var clientOrder = "GOOG 300 542.0 B, AAPL 50 145.0 B, CSCO 250.0 29 B, GOOG 200 580.0 S"
+        var badFormatedString = listOf("CSCO 250.0 29 B")
+        assertEquals(badFormatedString, OrdersSummary.findWrongFormattedOrders(clientOrder))
+
+        clientOrder = "GOOG 90 160.45 B, JPMC 67 12.8 S, MYSPACE 24.0 210 B, CITI 50 450 B, CSCO 100 55.5 S"
+        badFormatedString = listOf("MYSPACE 24.0 210 B", "CITI 50 450 B")
+        assertEquals(badFormatedString, OrdersSummary.findWrongFormattedOrders(clientOrder))
+
+        clientOrder = "ZNGA 1300 2.66 B, CLH15.NYM 50 56.32 B, OWW 1000 11.623 B, OGG 20 580.1 B"
+        badFormatedString = listOf()
+        assertEquals(badFormatedString, OrdersSummary.findWrongFormattedOrders(clientOrder))
+    }
+
+    @Test
+    fun chooseRightOrderTest() {
+
+        var clientOrder = "GOOG 300 542.0 B, AAPL 50 145.0 B, CSCO 250.0 29 B, GOOG 200 580.0 S"
+        var rightFormattedOrders = listOf("GOOG 300 542.0 B", "AAPL 50 145.0 B", "GOOG 200 580.0 S")
+        assertEquals(rightFormattedOrders, OrdersSummary.chooseRightOrders(clientOrder))
+
+        clientOrder = "GOOG 90 160.45 B, JPMC 67 12.8 S, MYSPACE 24.0 210 B, CITI 50 450 B, CSCO 100 55.5 S"
+        rightFormattedOrders = listOf("GOOG 90 160.45 B", "JPMC 67 12.8 S", "CSCO 100 55.5 S")
+        assertEquals(rightFormattedOrders, OrdersSummary.chooseRightOrders(clientOrder))
+
+    }
+
+    @Test
+    fun buyAndSellTest() {
+        var clientOrders = "GOOG 300 542.0 B, AAPL 50 145.0 B, CSCO 250.0 29 B, GOOG 200 580.0 S"
+        var buyAndSellResult = "Buy: 169850 Sell: 116000"
+        assertEquals(buyAndSellResult, OrdersSummary.buyAndSellSummary(clientOrders))
+
+        clientOrders = "ZNGA 1300 2.66 B, CLH15.NYM 50 56.32 B, OWW 1000 11.623 B, OGG 20 580.1 B"
+        buyAndSellResult = "Buy: 29499 Sell: 0"
+        assertEquals(buyAndSellResult, OrdersSummary.buyAndSellSummary(clientOrders))
+
+
+        clientOrders = "GOOG 90 160.45 B, JPMC 67 12.8 S, MYSPACE 24.0 210 B, CITI 50 450 B, CSCO 100 55.5 S"
+        buyAndSellResult = "Buy: 14440 Sell: 6408"
+        assertEquals(buyAndSellResult, OrdersSummary.buyAndSellSummary(clientOrders))
+
+
+    }
+
+
 }
