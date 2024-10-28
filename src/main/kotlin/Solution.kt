91561aa7bf4e7ee0f48ce496ace9db0621811a83
Jacek programmer <jacek301@gmail.com>
2024-10-28 22:00:31 +0100

    Unwanted dollars kata my solution

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,7 +1,18 @@
 package com.codewars.geoffp
 
 object UnwantedDollars {
-    fun moneyValue(money:String):Double {
-        return 0.0
+    fun moneyValue(money: String): Double {
+
+        if (!money.contains("\\d+".toRegex())){
+            return 0.0
+        }
+
+        if (money.contains("$")) {
+            return money.replace("$", "")
+                .replace(" ","").toDouble()
+        }
+
+
+        return money.toDouble()
     }
 }
