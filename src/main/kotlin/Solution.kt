63a4fd43b43b6b33e5ce649d4d484060acd23827
Jacek programmer <jacek301@gmail.com>
2024-10-29 17:16:37 +0100

    Word values kata my solution

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,8 +1,25 @@
 object Solution {
 
+    private val alphabetValueMap: Map<Char, Int>
+
+    init {
+        val alphabet = ('a'..'z').toList()
+        alphabetValueMap = alphabet.mapIndexed { index, char -> char to index + 1 }.toMap()
+    }
+
     fun nameValue(arr: Array<String>): IntArray {
-        // your clean code here
-        TODO()
+
+        return arr.mapIndexed { index, word ->
+
+            (index + 1) * valueFromWord(word)
+
+        }.toIntArray()
+
+    }
+
+    private fun valueFromWord(word: String): Int {
+
+        return word.map { char -> alphabetValueMap.getOrDefault(char, 0) }.sum()
     }
 
 }
