4b7dc874a3aab6a42f245e6e9079bfe51b91700c
Jacek programmer <jacek301@gmail.com>
2024-11-03 18:31:57 +0100

    Color Choice kata start

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,53 +1,6 @@
-package solution
+package color
 
-object MatchSubst {
+fun checkchoose(m:Long, n:Int):Long {
 
-    fun change(s: String, prog: String, version: String): String {
-
-        var phoneNumber = getElementFromLine(getPhoneLine(s))
-        if (!isPhoneValid(phoneNumber)) {
-            return "ERROR: VERSION or PHONE"
-        } else {
-            phoneNumber = "+1-503-555-0090"
-        }
-
-        var versionNumber = getElementFromLine(getVersionLine(s))
-        if (!isVersionValid(versionNumber)) {
-            return "ERROR: VERSION or PHONE"
-        } else {
-            if (versionNumber != "2.0") {
-                versionNumber = version
-            }
-
-        }
-
-        return "Program: $prog Author: g964 Phone: $phoneNumber Date: 2019-01-01 Version: $versionNumber"
-    }
-
-    fun isPhoneValid(phone: String): Boolean {
-        return phone.matches("\\+1-\\d{3}-\\d{3}-\\d{4}".toRegex())
-    }
-
-    fun isVersionValid(version: String): Boolean {
-        return version.matches("\\d+\\.\\d+".toRegex())
-    }
-
-    fun getPhoneLine(input: String): String {
-        return "Phone:.*".toRegex().find(input)?.value ?: ""
-    }
-
-    fun getVersionLine(input: String): String {
-        return "Version:.*".toRegex().find(input)?.value ?: ""
-    }
-
-    fun getElementFromLine(line: String): String {
-        val parts = line.split(":")
-
-        return if (parts.size > 1) {
-            parts[1].trim()
-        } else {
-            ""
-        }
-    }
-
+    return  1L
 }
--- src/test/kotlin/TestSolution.kt
+++ src/test/kotlin/TestSolution.kt
@@ -1,99 +1,17 @@
-package solution
+package color
 
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Test
-import kotlin.test.assertTrue
 
-
-class MatchSubstTest {
-    private fun dotest(s: String, prog: String, version: String, exp: String) {
-        val ans = MatchSubst.change(s, prog, version)
-        assertEquals(exp, ans)
-    }
+class CheckchooseTest {
 
     @Test
-    fun test() {
-        println("Basic Tests");
-        val s1 = "Program title: Primes\nAuthor: Kern\nCorporation: Gold\nPhone: +1-503-555-0091\nDate: Tues April 9, 2005\nVersion: 6.7\nLevel: Alpha"
-        dotest(s1, "Ladder", "1.1", "Program: Ladder Author: g964 Phone: +1-503-555-0090 Date: 2019-01-01 Version: 1.1")
-
-        val s14 = "Program title: Primes\nAuthor: Kern\nCorporation: Gold\nPhone: +1-503-555-0091\nDate: Tues April 9, 2005\nVersion: 6.7.5\nLevel: Alpha"
-        dotest(s14, "Ladder", "1.1", "ERROR: VERSION or PHONE")
-
-        val s2 = "Program title: Primes\nAuthor: Kern\nCorporation: Gold\nPhone: +1-503-555-0091\nDate: Tues April 9, 2005\nVersion: 2.0\nLevel: Alpha"
-        dotest(s2, "Ladder", "1.1", "Program: Ladder Author: g964 Phone: +1-503-555-0090 Date: 2019-01-01 Version: 2.0")
+    fun BasicTests() {
+        println("****** Basic Tests ******")
+        assertEquals(2, checkchoose(6, 4))
+        assertEquals(1, checkchoose(4, 4))
+        assertEquals(3, checkchoose(35, 7))
 
     }
-
-    @Test
-    fun positivistCase() {
-
-        val s1 = "Program title: Primes\nAuthor: Kern\nCorporation: Gold\nPhone: +1-503-555-0091\nDate: Tues April 9, 2005\nVersion: 6.7\nLevel: Alpha"
-        val expected = "Program: Ladder Author: g964 Phone: +1-503-555-0090 Date: 2019-01-01 Version: 1.1"
-        dotest(s1, "Ladder", "1.1", expected)
-
-    }
-
-    @Test
-    fun phoneValidFormatTest() {
-        assertTrue(MatchSubst.isPhoneValid("+1-503-555-0090"),"Phone is not in valid form")
-        assertFalse(MatchSubst.isPhoneValid("+1-50-55-0090"),"Phone is not valid")
-
-    }
-
-    @Test
-    fun versionValidFormatTest() {
-        assertTrue(MatchSubst.isVersionValid("14.275"),"Version valid")
-        assertFalse(MatchSubst.isVersionValid(".9"),"Version is not Valid")
-        assertFalse(MatchSubst.isVersionValid("7"),"Version is not Valid")
-    }
-
-    @Test
-    fun getPhoneLineTest() {
-        val s1 = "Program title: Primes\n" +
-                "Author: Kern\n" +
-                "Corporation: Gold\n" +
-                "Phone: +1-503-555-0091\n" +
-                "Date: Tues April 9, 2005\n" +
-                "Version: 6.7\n" +
-                "Level: Alpha"
-
-        assertEquals("Phone: +1-503-555-0091",MatchSubst.getPhoneLine(s1))
-    }
-
-    @Test
-    fun getVersionLineTest() {
-        val s1 = "Program title: Primes\n" +
-                "Author: Kern\n" +
-                "Corporation: Gold\n" +
-                "Phone: +1-503-555-0091\n" +
-                "Date: Tues April 9, 2005\n" +
-                "Version: 6.7\n" +
-                "Level: Alpha"
-
-        assertEquals("Version: 6.7",MatchSubst.getVersionLine(s1))
-    }
-
-    @Test
-    fun getElementFromLineTest() {
-        assertEquals("+1-503-555-0091",MatchSubst.getElementFromLine("Phone: +1-503-555-0091"))
-        assertEquals("",MatchSubst.getElementFromLine("Phone"))
-
-    }
-
-    @Test
-    fun getVersionFromLineTest() {
-        assertEquals("6.7",MatchSubst.getElementFromLine("Version: 6.7"))
-        assertEquals("",MatchSubst.getElementFromLine("version"))
-
-    }
-
-
-
-
-
-
-
 
 }
