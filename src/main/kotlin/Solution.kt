a10d489d1e44ddef7f2ce1c0e60cace7337ac728
Jacek programmer <jacek301@gmail.com>
2024-11-22 22:18:59 +0100

    Playing on a chessboard working to certain point. But for very long numbers works to long or out of memory. Starting point to improvement

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,7 +1,57 @@
 package solution
 
+import java.math.BigInteger
+
 object Suite2 {
     fun game(n: Long): String {
-        return ""
+
+        val denominators: MutableList<Long> = mutableListOf()
+        val numerators: MutableList<Long> = mutableListOf()
+        for (rowPosition in 1L..n) {
+
+            numerators.add(rowPosition)
+
+            for (columnPosition in 1L..n) {
+
+                denominators.add(rowPosition + columnPosition)
+
+            }
+
+        }
+
+        val leastCommonMultiply = findLeastCommonMultipleMany(denominators)
+        val numeratorsMultipliers = denominators.map { leastCommonMultiply / it.toBigInteger() }.toList()
+        val numeratorSum =
+            numeratorsMultipliers.foldIndexed(BigInteger.ZERO) { index, numeratorsSum, multiplier ->
+                numeratorsSum + (numerators[index % numerators.size].toBigInteger() * multiplier)
+            }
+
+        val gcd = findGreatestCommonDivisor(numeratorSum, leastCommonMultiply)
+
+        val numeratorShortened = numeratorSum / gcd
+        val denominatorShortened = leastCommonMultiply / gcd
+
+        if (denominatorShortened == BigInteger.ONE) {
+            return "[${numeratorShortened.toLong()}]"
+        }
+
+        return "[${numeratorShortened.toLong()}, ${denominatorShortened.toLong()}]"
+
+    }
+
+    fun findGreatestCommonDivisor(denominator: BigInteger, otherDenominator: BigInteger): BigInteger {
+        return if (otherDenominator == BigInteger.ZERO) denominator
+        else findGreatestCommonDivisor(otherDenominator, denominator % otherDenominator)
+
+    }
+
+    fun findLeastCommonMultiple(denominator: BigInteger, otherDenominator: BigInteger): BigInteger {
+        return denominator / findGreatestCommonDivisor(denominator, otherDenominator) * otherDenominator
+    }
+
+    fun findLeastCommonMultipleMany(listOfDenominators: List<Long>): BigInteger {
+        return listOfDenominators.fold(BigInteger.ONE) { lcm, denominator ->
+            findLeastCommonMultiple(lcm, denominator.toBigInteger())
+        }
     }
 }
--- src/test/kotlin/TestSolution.kt
+++ src/test/kotlin/TestSolution.kt
@@ -2,9 +2,19 @@ package solution
 
 
 import org.junit.jupiter.api.Test
+import java.math.BigInteger
 import kotlin.test.assertEquals
 
 class Suite2Test {
+
+    @Test
+    fun veryBigNumbersTest() {
+
+        assertEquals("[20808]", Suite2.game(204))
+        assertEquals("[651249, 2]", Suite2.game(807))   // tutaj bardzo długo ciekawe co poradzić ??
+//        assertEquals("[651249, 2]", Suite2.game(5014))  // tutaj out of memory
+
+    }
 
     @Test
     fun basicTests() {
@@ -12,4 +22,84 @@ class Suite2Test {
         assertEquals("[10201, 2]", Suite2.game(101))
 
     }
+
+    @Test
+    fun myBasicTest() {
+        assertEquals("[1, 2]", Suite2.game(1))
+        assertEquals("[2]", Suite2.game(2))
+        assertEquals("[9, 2]", Suite2.game(3))
+        assertEquals("[8]", Suite2.game(4))
+        assertEquals("[25, 2]", Suite2.game(5))
+        assertEquals("[50]", Suite2.game(10))
+        assertEquals("[200]", Suite2.game(20))
+        assertEquals("[441, 2]", Suite2.game(21))
+    }
+
+
+    @Test
+    fun findGreatestCommonDivisorTest() {
+        assertEquals(BigInteger.valueOf(2), Suite2.findGreatestCommonDivisor(BigInteger.TWO, BigInteger.TWO))
+        assertEquals(
+            BigInteger.valueOf(4),
+            Suite2.findGreatestCommonDivisor(BigInteger.valueOf(4), BigInteger.valueOf(4))
+        )
+        assertEquals(BigInteger.ONE, Suite2.findGreatestCommonDivisor(BigInteger.TWO, BigInteger.valueOf(7)))
+        assertEquals(
+            BigInteger.valueOf(4),
+            Suite2.findGreatestCommonDivisor(BigInteger.valueOf(8), BigInteger.valueOf(12))
+        )
+        assertEquals(
+            BigInteger.valueOf(4),
+            Suite2.findGreatestCommonDivisor(BigInteger.valueOf(12), BigInteger.valueOf(8))
+        )
+        assertEquals(BigInteger.TWO, Suite2.findGreatestCommonDivisor(BigInteger.valueOf(86), BigInteger.valueOf(16)))
+        assertEquals(BigInteger.TWO, Suite2.findGreatestCommonDivisor(BigInteger.TWO, BigInteger.ZERO))
+        assertEquals(BigInteger.ONE, Suite2.findGreatestCommonDivisor(BigInteger.TWO, BigInteger.ONE))
+
+    }
+
+    @Test
+    fun findLeastCommonMultipleForTwoDenominatorsTest() {
+
+        assertEquals(BigInteger.TWO, Suite2.findLeastCommonMultiple(BigInteger.TWO, BigInteger.TWO))
+        assertEquals(BigInteger.valueOf(4), Suite2.findLeastCommonMultiple(BigInteger.valueOf(4), BigInteger.TWO))
+        assertEquals(
+            BigInteger.valueOf(6),
+            Suite2.findLeastCommonMultiple(BigInteger.valueOf(6), BigInteger.valueOf(2))
+        )
+        assertEquals(
+            BigInteger.valueOf(14),
+            Suite2.findLeastCommonMultiple(BigInteger.valueOf(7), BigInteger.valueOf(2))
+        )
+        assertEquals(
+            BigInteger.valueOf(26),
+            Suite2.findLeastCommonMultiple(BigInteger.valueOf(26), BigInteger.valueOf(13))
+        )
+        assertEquals(
+            BigInteger.valueOf(26),
+            Suite2.findLeastCommonMultiple(BigInteger.valueOf(13), BigInteger.valueOf(26))
+        )
+        assertEquals(
+            BigInteger.valueOf(2),
+            Suite2.findLeastCommonMultiple(BigInteger.valueOf(2), BigInteger.valueOf(1))
+        )
+        assertEquals(
+            BigInteger.valueOf(0),
+            Suite2.findLeastCommonMultiple(BigInteger.valueOf(2), BigInteger.valueOf(0))
+        )
+
+    }
+
+
+    @Test
+    fun findLeastCommonMultipleForManyDenominatorsTest() {
+        assertEquals(BigInteger.valueOf(4), Suite2.findLeastCommonMultipleMany(listOf(2, 4, 2)))
+        assertEquals(BigInteger.valueOf(360), Suite2.findLeastCommonMultipleMany(listOf(4, 6, 8, 12, 15, 18, 20, 24)))
+        assertEquals(BigInteger.valueOf(12), Suite2.findLeastCommonMultipleMany(listOf(2, 3, 3, 4)))
+        assertEquals(BigInteger.valueOf(2), Suite2.findLeastCommonMultipleMany(listOf(2)))
+
+    }
+
+
 }
+
