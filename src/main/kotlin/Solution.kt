23a9e50c08465528a7084fa213c72c0dfe5e513d
Jacek programmer <jacek301@gmail.com>
2024-11-25 23:01:59 +0100

    Moves in squared strings (I) kata add function argument and fake return string

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -4,11 +4,15 @@ object Opstrings {
 
     fun vertMirror(strng: String): String {
         // your code
+        return ""
     }
     fun horMirror(strng: String): String {
         // your code
+      return  ""
     }
-    fun oper(... , s: String): String {
+    fun oper(fct: (String) -> String  , s: String): String {
         // your code and complete ... with a function to apply to s
+
+        return ""
     }
 }
