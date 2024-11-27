d31fa96abe14574544b72f8a63fa3c0486d303d0
Jacek programmer <jacek301@gmail.com>
2024-11-27 18:28:50 +0100

    Moves in squared strings (I) kata my solution

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -3,16 +3,27 @@ package solution
 object Opstrings {
 
     fun vertMirror(strng: String): String {
-        // your code
-        return ""
+        return strng.split("\n").joinToString("\n") { it.reversed() }
     }
-    fun horMirror(strng: String): String {
-        // your code
-      return  ""
-    }
-    fun oper(fct: (String) -> String  , s: String): String {
-        // your code and complete ... with a function to apply to s
 
-        return ""
+    fun horMirror(strng: String): String {
+
+        val delimiter = "\n"
+        val centerDivision = strng.split(delimiter).count() / 2
+
+        val firstNewLineIndex = strng.indexOf(delimiter)
+        val centerNewLineIndex =  strng.indexOf(delimiter, firstNewLineIndex + centerDivision -1)
+
+        val firstHalf = strng.substring(0,centerNewLineIndex)
+        val secondHalf = strng.substring(centerNewLineIndex)
+
+        val secondHalfReversed = secondHalf.split(delimiter).reversed().joinToString(delimiter)
+        val firstHalfReversed = firstHalf.split(delimiter).reversed().joinToString(delimiter)
+
+        return secondHalfReversed + firstHalfReversed
+    }
+
+    fun oper(fct: (String) -> String, s: String): String {
+        return fct(s)
     }
 }
