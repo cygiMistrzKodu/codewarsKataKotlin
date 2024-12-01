7fb35187c42e627ce9f3b72d62f007305786a682
Jacek programmer <jacek301@gmail.com>
2024-12-01 19:46:47 +0100

    Moduli number system kata solution

--- src/test/kotlin/TestSolution.kt
+++ src/test/kotlin/TestSolution.kt
@@ -14,8 +14,8 @@ class  ModSystemTest {
         testing(779, intArrayOf(8,7,5,3), "-3--2--4--2-")
         testing(187, intArrayOf(8,7,5,3), "-3--5--2--1-")
         testing(259, intArrayOf(8,7,5,3), "-3--0--4--1-")
-        testing(15, intArrayOf(8,6,5,3), "Not applicable")
         testing(15, intArrayOf(3, 2), "Not applicable")
+        testing(15, intArrayOf(8,6,5,3), "Not applicable")
 
     }
 }
