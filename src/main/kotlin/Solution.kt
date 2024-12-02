48a5f5050d2156dc4d75a9341ab1143b4b09ba0d
Jacek programmer <jacek301@gmail.com>
2024-12-02 02:59:19 +0100

    Mexican Wave kata my solution

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,3 +1,23 @@
 fun wave(str: String): List<String> {
-    return emptyList()
+
+    if (str.isEmpty()) {
+        return emptyList()
+    }
+
+    val mexicanWave = mutableListOf<String>()
+
+    if (str.count() <= 1) {
+        mexicanWave.add(str.uppercase())
+        return mexicanWave
+    }
+
+
+    for ((index, _) in str.withIndex()) {
+
+        if (str[index].isLetter()) {
+            mexicanWave.add(str.substring(0, index) + str[index].uppercase() + str.substring(index + 1))
+        }
+
+    }
+    return mexicanWave
 }
--- src/test/kotlin/TestSolution.kt
+++ src/test/kotlin/TestSolution.kt
@@ -1,5 +1,3 @@
-
-
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
@@ -7,8 +5,35 @@ class TestKata {
     @Test
     fun basicTests() {
         assertEquals(listOf("A       b    ", "a       B    "), wave("a       b    "))
-        assertEquals(listOf("This is a few words", "tHis is a few words", "thIs is a few words", "thiS is a few words", "this Is a few words", "this iS a few words", "this is A few words", "this is a Few words", "this is a fEw words", "this is a feW words", "this is a few Words", "this is a few wOrds", "this is a few woRds", "this is a few worDs", "this is a few wordS"), wave("this is a few words"))
+        assertEquals(
+            listOf(
+                "This is a few words",
+                "tHis is a few words",
+                "thIs is a few words",
+                "thiS is a few words",
+                "this Is a few words",
+                "this iS a few words",
+                "this is A few words",
+                "this is a Few words",
+                "this is a fEw words",
+                "this is a feW words",
+                "this is a few Words",
+                "this is a few wOrds",
+                "this is a few woRds",
+                "this is a few worDs",
+                "this is a few wordS"
+            ), wave("this is a few words")
+        )
         assertEquals(listOf<String>(), wave(""))
+        assertEquals(listOf(" Gap ", " gAp ", " gaP "), wave(" gap "))
+    }
+
+    @Test
+    fun myTest() {
+        assertEquals(listOf<String>(), wave(""))
+        assertEquals(listOf("A"), wave("a"))
+        assertEquals(listOf("Ab", "aB"), wave("ab"))
+        assertEquals(listOf("Abc", "aBc", "abC"), wave("abc"))
         assertEquals(listOf(" Gap ", " gAp ", " gaP "), wave(" gap "))
     }
 }
