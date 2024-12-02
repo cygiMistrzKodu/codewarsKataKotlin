dcc898c06574669803be1dc81a2c3d1b12c85d81
Jacek programmer <jacek301@gmail.com>
2024-12-02 22:55:33 +0100

    Alternate capitalization kata my solution

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,6 +1,26 @@
 package altcaps
 
 fun capitalize(text: String): List<String> {
-    //TODO: Add your code!
-    return listOf()
+
+    val evenCapitalization = makeUppercaseLetters(text) { a -> a % 2 == 0 }
+    val oddCapitalization = makeUppercaseLetters(text) { a -> a % 2 == 1 }
+
+    return listOf(evenCapitalization, oddCapitalization)
+}
+
+fun makeUppercaseLetters(text: String, isLetterIndexToCapitalize: (Int) -> Boolean): String {
+
+    val capitalization = mutableListOf<String>()
+
+    for ((index, _) in text.withIndex()) {
+
+        if (isLetterIndexToCapitalize(index)) {
+            capitalization.add(text[index].uppercase())
+        } else {
+            capitalization.add(text[index].toString())
+        }
+
+    }
+
+    return capitalization.joinToString("")
 }
