dc2ad7e3d73dfbb30b969c0a5aa30530cfe47d89
Jacek programmer <jacek301@gmail.com>
2024-12-09 23:25:04 +0100

    Diophantine Equation kata my solution

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,6 +1,46 @@
 package diophequa
 
-fun solEquaStr(n:Long):String {
+fun solEquaStr(n: Long): String {
 
-    return  ""
+    val factorsPairs = findFactors(n)
+
+    val solutionsNonNegative = mutableListOf<Pair<Long, Long>>()
+    for (pair in factorsPairs) {
+        val sumPairs = pair.first + pair.second
+        val x = sumPairs.toDouble() / 2
+
+        if (isInteger(x)) {
+
+            val y = (-pair.first + x) / 2
+
+            if (isInteger(y)) {
+
+                if (x >= 0 && y >= 0) {
+                    solutionsNonNegative.add(Pair(x.toLong(), y.toLong()))
+                }
+            }
+
+        }
+    }
+
+    return "${solutionsNonNegative.map { listOf(it.first, it.second) }}"
 }
+
+fun findFactors(number: Long): List<Pair<Long, Long>> {
+    val factors = mutableListOf<Pair<Long, Long>>()
+    for (i in 1L..number) {
+        if (number % i == 0L) {
+            val pair = Pair(i, number / i)
+            factors.add(pair)
+        }
+    }
+    return factors
+}
+
+fun isInteger(number: Double): Boolean {
+    return number % 1.0 == 0.0
+}
+
+
+
+
--- src/test/kotlin/TestSolution.kt
+++ src/test/kotlin/TestSolution.kt
@@ -3,7 +3,8 @@ package diophequa
 
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
-import java.util.Random
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 class diophanteTest {
     @Test
@@ -22,5 +23,42 @@ class diophanteTest {
             "[[4505, 2252], [1503, 750], [647, 320], [505, 248], [415, 202], [353, 170], [225, 102], [153, 60], [135, 48], [103, 20], [97, 10], [95, 2]]"
         assertEquals(a, solEquaStr(9009))
     }
+
+    @Test
+    fun testWithNumber16() {
+        assertEquals("[[4, 0]]", solEquaStr(16))
+    }
+
+    @Test
+    fun testWhenNoSolutions() {
+        val a =
+            "[]"
+        assertEquals(a, solEquaStr(9002))
+    }
+
+    @Test
+    fun findFactorsForNumbersTest() {
+
+        val expectedPairs = listOf(Pair(1L, 12L), Pair(2L, 6L), Pair(3L, 4L), Pair(4L, 3L), Pair(6L, 2L), Pair(12L, 1L))
+        assertEquals(expectedPairs, findFactors(12L))
+
+        val expectedPairs2 = listOf(Pair(1L, 6L), Pair(2L, 3L), Pair(3L, 2L), Pair(6L, 1L))
+        assertEquals(expectedPairs2, findFactors(6L))
+
+    }
+
+    @Test
+    fun iasIntegerTest() {
+        assertTrue(isInteger(4.0))
+        assertTrue(isInteger(8976.0))
+        assertFalse(isInteger(4.5))
+        assertFalse(isInteger(987.12))
+        assertFalse(isInteger(1.02))
+
+    }
+
+
+
+
 }
 
